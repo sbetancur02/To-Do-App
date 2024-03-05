78a21011abb25d0122c78b798020e8eb5ab23602
sbetancur02 <158582561+sbetancur02@users.noreply.github.com>
2024-03-05 18:45:56 -0500

    Update deploy.sh

--- app/deploy.sh
+++ app/deploy.sh
@@ -1,3 +1,3 @@
-if docker ps -a --format '{{.Name}}' | grep -q "^sbetancur02/to-do-app\$"; then
+if [ -z "$(docker ps -qf "ancestor=sbetancur02/to-do-app")"] then
     docker stop $(docker ps -qf ancestor="sbetancur02/to-do-app")
 fi
