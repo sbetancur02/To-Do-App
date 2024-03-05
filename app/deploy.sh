b656f97b11b6ebe24efcbf26e6df34be74b9dfed
Simon Betancur Bedoya <Simon.Betancur@endava.com>
2024-03-05 16:57:54 -0500

    Add deploy.sh file

---
+++ app/deploy.sh
@@ -0,0 +1,7 @@
+image_name = "sbetancur02/to-do-app"
+
+container_id = $(docker ps -qf ancestor="$image_name")
+
+if docker ps -a --format '{{.Name}}' | grep -q "^$image_name\$"; then
+
+    docker stop "$container_id"
