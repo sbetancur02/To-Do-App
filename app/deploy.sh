c131ffb7a97872f67dce381aad5f63b3a31c6e95
sbetancur02 <158582561+sbetancur02@users.noreply.github.com>
2024-03-05 20:14:42 -0500

    Update deploy.sh

--- app/deploy.sh
+++ app/deploy.sh
@@ -1,3 +1,3 @@
-if [ -z "$(docker ps -qf "ancestor=sbetancur02/to-do-app")"] then
-    docker stop $(docker ps -qf ancestor="sbetancur02/to-do-app")
+if sudo docker inspect --format '{{.State.Running}}' "$(sudo docker ps -qf ancestor="sbetancur02/to-do-app")" 2>dev/null | grep -q "true"; then
+    sudo docker stop $(sudo docker ps -qf ancestor="sbetancur02/to-do-app")
 fi
